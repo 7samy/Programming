1b0f3cd79cc40a1e113e45c2362ae0102ea6c0c2
7samy <hartmannsamy1@proton.me>
2026-04-23 21:02:12 +0200

    auto: sync 2026-04-23 21:02

--- CPP/Game_Character/Game_Character.h
+++ CPP/Game_Character/Game_Character.h
@@ -11,18 +11,8 @@ class GameCharacter {
   GameCharacter* m_buddy;
 
   public:
-  GameCharacter(std::string m_name, int m_health, GameCharacter* m_buddy = nullptr)
-  : m_name(m_name), m_health(m_health), m_buddy(m_buddy)
-  { 
-  }
-
-  ~GameCharacter(){
-    std::cout << "Destroy: " << m_name << std::endl;
-    if (m_buddy != nullptr){
-      std::cout << "Free buddy: " << m_buddy->m_name << std::endl;
-      delete m_buddy;
-      m_buddy = nullptr;
-    }
-  }
+  GameCharacter(std::string m_name, int m_health, GameCharacter* m_buddy = nullptr);
+  ~GameCharacter();
+  void takeDamage(int amount);
 
 };
